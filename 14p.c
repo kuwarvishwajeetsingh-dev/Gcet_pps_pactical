a3ad5f3c7d03665e2fbf8c982541ef13383a2144
Kuwar Vishwajeet Singh <kuwarvishwajeetsingh@gmail.com>
2026-03-16 16:57:54 +0530

    Fix prime number check logic in 14p.c

--- 14p.c
+++ 14p.c
@@ -5,10 +5,11 @@ printf("Enter a number: ");
 scanf("%d",&a);
 if(a==2){
     printf("%d is a prime number",a);
-    else if(a=1){
-                  printf("%d is not a prime number",a);
-                }
-else{
+}
+else if(a==1){
+             printf("%d is not a prime number",a);
+           }
+else {
 for(int i=2;i<=a;i++){
     if((a%i==0)&&(i<=a/2)){
         printf("%d is not a prime number",a);
@@ -20,7 +21,7 @@ for(int i=2;i<=a;i++){
         break;
     }
 
-}
-}
+                  }
+    }
     return 0;
 }
